a3753d59076a68539fb69c2c53740e2c204703d5
Esteban Forero <estebanffm@outlook.com>
2024-04-27 17:15:13 -0500

    Co-authored-by: Joel-109 <Joel-109@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}> 
+        <App />
+      </ClerkProvider>
+      <ReactQueryDevtools initialIsOpen={true}/>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
